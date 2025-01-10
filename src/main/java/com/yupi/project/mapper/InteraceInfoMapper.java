cb9638cdc99a4a73e7a3daaee5defa745e0919ae
lms <1540288243@qq.com>
2025-01-10 15:25:50 +0800

    初始化提交

---
+++ src/main/java/com/yupi/project/mapper/InteraceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.yupi.project.model.entity.InteraceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author LMS
+* @description 针对表【interace_info(接口信息)】的数据库操作Mapper
+* @createDate 2025-01-10 15:20:13
+* @Entity com.yupi.project.model.entity.InteraceInfo
+*/
+public interface InteraceInfoMapper extends BaseMapper<InteraceInfo> {
+
+}
+
+
+
+
